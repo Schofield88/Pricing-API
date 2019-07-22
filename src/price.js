7a040719ec45f9cbd98273aa0612f7ef6633c031
Luke Phyall <shoreditchwarlock@gmail.com>
2019-07-22 10:45:18 +0100

    DRYs Price out

--- src/price.js
+++ src/price.js
@@ -1,28 +1,18 @@
 const fs = require('fs');
 
 class Price {
-  getUnitPrice(element, pricing) {
-    const item = element;
-    const pricingData = pricing.prices;
-
-    const unitPrice = pricingData.find(prices => prices.product_id === item.product_id);
-
-    return unitPrice.price;
-  }
-
-  getUnitVat(element, pricing) {
+  getUnitPriceAndVat(element, pricing) {
     const item = element;
     const pricingData = pricing;
 
-    const priceStuffForThisOne = pricingData.prices.find(
-      price => price.product_id === item.product_id,
-    );
-    const unitPrice = priceStuffForThisOne.price;
-    const vatBand = priceStuffForThisOne.vat_band;
+    const itemPricingInfo = pricingData.prices.find(price => price.product_id === item.product_id);
+    const unitPrice = itemPricingInfo.price;
+
+    const vatBand = itemPricingInfo.vat_band;
     const rate = pricingData.vat_bands[vatBand];
     const vat = Math.round(unitPrice * rate);
 
-    return vat;
+    return { vat, unitPrice };
   }
 
   price(itemsArray) {
@@ -32,8 +22,9 @@ class Price {
     // remaps the array of order items to include pricing data
     const itemsWithPrice = items.map((item) => {
       const newEntry = item;
-      newEntry.unit_price = this.getUnitPrice(item, pricingData);
-      newEntry.unit_vat = this.getUnitVat(item, pricingData);
+      const unitPriceAndVat = this.getUnitPriceAndVat(item, pricingData);
+      newEntry.unit_price = unitPriceAndVat.unitPrice;
+      newEntry.unit_vat = unitPriceAndVat.vat;
       return newEntry;
     });
 
